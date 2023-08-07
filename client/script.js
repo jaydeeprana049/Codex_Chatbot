9f2e2155fa89a05a16ed958729ac2b05ac74b94e
jaydeeprana049 <jaydeeprana049@gmail.com>
2023-08-08 00:56:39 +0530

    done

--- client/script.js
+++ client/script.js
@@ -75,7 +75,7 @@ const handleSubmit = async (e) => {
   //call the loader function
   loader(messageDiv);
   //fetch the response from the server
-  const response = await fetch('https://localhost:5000', {
+  const response = await fetch('https://codex-cg1x.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
